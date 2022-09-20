4eef35407c05b59825e4dc9302a7a38a1f419b45
Murachue <392264+murachue@users.noreply.github.com>
2022-09-20 23:53:04 +0900

    preview

--- pages/components/app.tsx
+++ pages/components/app.tsx
@@ -24,7 +24,7 @@ const App = () => {
                     </div>
                 </TmplNode>
             </div>
-            {/* <DraggingPreview /> */}
+            <DraggingPreview />
         </DndProvider>
     </div >;
 };
--- pages/components/dragpreview.tsx
+++ pages/components/dragpreview.tsx
@@ -7,14 +7,17 @@ const DraggingPreview = () => {
     const { type, item, offset } = useDragLayer(monitor => ({
         type: monitor.getItemType(),
         item: monitor.getItem(),
-        offset: monitor.getClientOffset(),
+        offset: monitor.getDifferenceFromInitialOffset(),
     }));
 
-    useEffect(() => console.log({ type, item, offset }), [type, item, offset]);
+    useEffect(() => console.dir({ type, item, offset }), [type, item, offset]);
 
-    return <div style={!offset ? { display: "none" } : { position: "absolute", left: 0, top: 0, width: "100%", height: "100%", pointerEvents: "none" }}>
-        <div style={!offset ? {} : { transform: `translate(${offset.x},${offset.y})` }}>
-            {({ "InstNode": <InstNode {...item} />, "TmplNode": <TmplNode {...item} /> } as any)[type!]}
+    return <div style={{ position: "absolute", left: 0, top: 0, width: "100%", height: "100%", pointerEvents: "none" }}>
+        <div style={{ width: "min-content", ...(!offset ? {} : { transform: `translate(${offset.x}px,${offset.y}px)` }) }}>
+            {({
+                "InstNode": <InstNode {...item} preview />,
+                "TmplNode": <TmplNode {...item} preview />,
+            } as any)[type!]}
         </div>
     </div>;
 };
