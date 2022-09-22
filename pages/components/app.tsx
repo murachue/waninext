3b49856e99f106c7b303caecc34d41e8d340dd35
Murachue <392264+murachue@users.noreply.github.com>
2022-09-23 01:08:48 +0900

    experimental touch support

--- pages/components/app.tsx
+++ pages/components/app.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { TouchBackend } from "react-dnd-touch-backend";
@@ -8,14 +8,17 @@ import DraggingPreview from "./dragpreview";
 import TmplNode from "./tmplnode";
 
 const App = () => {
+    const [width, setWidth] = useState(800);
 
     useEffect(() => {
+        setWidth(window.innerWidth);
+
         const context = new AudioContext({ sampleRate: 44010 });
         return () => { context.close(); };
     }, []);
 
     return <div id="app">
-        <DndProvider backend={HTML5Backend}>
+        <DndProvider backend={width < 800 ? TouchBackend : HTML5Backend}>
             <Desk />
             <div style={{
                 position: "absolute",
